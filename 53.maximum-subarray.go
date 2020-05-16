5f587dd2ea6450d86e4f21bcc596db2f2120f095
livepo <nofity@livepo.info>
2020-05-16 09:31:39 +0800

    ac 53

--- 53.maximum-subarray.go
+++ 53.maximum-subarray.go
@@ -1,3 +1,33 @@
-func maxSubArray(nums []int) int {
+package main
+import "fmt"
 
+
+func maxSubArray(nums []int) int {
+	lenn := len(nums)
+	if lenn == 1 {
+		return nums[0]
+	}
+
+	DP := make([]int, lenn)
+	DP[0] = nums[0]
+	max := DP[0]
+
+	for i := 1; i < lenn; i++ {
+		if DP[i-1] <= 0 {
+			DP[i] = nums[i]
+		} else {
+			DP[i] = DP[i-1] + nums[i]
+		}
+		if DP[i] > max {
+			max = DP[i]
+		}
+	}
+	return max
+}
+
+
+func main() {
+	nums := []int{-2,1,-3,4,-1,2,1,-5,4}
+	output := maxSubArray(nums)
+	fmt.Println(output)
 }
