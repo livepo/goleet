46305d25b60ff55f0418199d6494c0bcf7cba2a2
livepo <nofity@livepo.info>
2020-05-16 10:15:02 +0800

    ac 69

--- 69.sqrtx.go
+++ 69.sqrtx.go
@@ -1,3 +1,22 @@
-func mySqrt(x int) int {
+package main
 
+import "fmt"
+
+
+func mySqrt(x int) int {
+	start := 4.0
+	for {
+		output := (start + float64(x) / start) / 2.0
+		if output - start >= -0.2 && output - start <= 0.2 {
+			return int(output)
+		}
+		start = output
+	}
+}
+
+
+
+func main() {
+	output := mySqrt(4)
+	fmt.Println(output)
 }
